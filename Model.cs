aa692e5db8fd729646a665bf0879b346ae934772
Meyer <meyer.mcmains@gmail.com>
2024-01-22 18:14:11 -0600

    rename AlbumId to Id

--- Model.cs
+++ Model.cs
@@ -11,7 +11,7 @@ namespace MusicBeePlugin.Model
 
     public partial class Album
     {
-        public string AlbumId { get; set; } = Guid.NewGuid().ToString();
+        public string Id { get; set; } = Guid.NewGuid().ToString();
 
         public string Title { get; set; }
 
